048e46781ba9cfca72a3028df52f3e2901572351
Kakaulin Aleksei <m9cnik17@yandex.ru>
2023-08-31 13:32:16 +0700

    сделал IoC для всех классов

--- hw05-aop/src/main/java/ru/otus/Demo.java
+++ hw05-aop/src/main/java/ru/otus/Demo.java
@@ -4,7 +4,7 @@ import ru.otus.proxy.Ioc;
 
 public class Demo {
     public static void main(String[] args) {
-        var testLogging = Ioc.createTestLogging();
+        var testLogging = (TestLogging) Ioc.createLoggedClass(new TestLoggingImpl());
 
         testLogging.calculation(124);
         testLogging.calculation(5435, 2213);
--- hw05-aop/src/main/java/ru/otus/proxy/Ioc.java
+++ hw05-aop/src/main/java/ru/otus/proxy/Ioc.java
@@ -2,17 +2,17 @@ package ru.otus.proxy;
 
 import ru.otus.Demo;
 import ru.otus.TestLogging;
-import ru.otus.TestLoggingImpl;
 
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Proxy;
 
 public class Ioc {
-    private Ioc() {}
+    private Ioc() {
+    }
 
-    public static TestLogging createTestLogging() {
-        InvocationHandler handler = new DemoInvocationHandler(new TestLoggingImpl());
-        return (TestLogging) Proxy.newProxyInstance(Demo.class.getClassLoader(),
+    public static Object createLoggedClass(Object clazz) {
+        InvocationHandler handler = new DemoInvocationHandler(clazz);
+        return Proxy.newProxyInstance(Demo.class.getClassLoader(),
                 new Class<?>[]{TestLogging.class}, handler);
     }
 }
