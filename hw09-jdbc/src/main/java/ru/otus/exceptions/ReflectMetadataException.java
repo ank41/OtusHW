8a316633ab0359a26ff7381d477acfabcead2754
Kakaulin Aleksei <m9cnik17@yandex.ru>
2023-10-05 18:04:06 +0700

    Необходимые классы и файлы

--- hw09-jdbc/src/main/java/ru/otus/exceptions/ReflectMetadataException.java
+++ hw09-jdbc/src/main/java/ru/otus/exceptions/ReflectMetadataException.java
@@ -1,9 +1,6 @@
 package ru.otus.exceptions;
 
 public class ReflectMetadataException extends RuntimeException{
-    public ReflectMetadataException(String message) {
-        super(message);
-    }
 
     public ReflectMetadataException(String message, Throwable cause) {
         super(message, cause);
