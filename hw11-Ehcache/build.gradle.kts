777be9e82753d97d545207ce87a4dc799e04e70f
Kakaulin Aleksei <m9cnik17@yandex.ru>
2023-10-16 18:42:30 +0700

    init

---
+++ hw11-Ehcache/build.gradle.kts
@@ -0,0 +1,28 @@
+plugins {
+    id("java")
+}
+
+group = "ru.otus"
+
+dependencies {
+    implementation("org.ehcache:ehcache")
+
+    implementation("ch.qos.logback:logback-classic")
+    implementation("org.hibernate.orm:hibernate-core")
+    implementation("org.flywaydb:flyway-core")
+
+    implementation("org.postgresql:postgresql")
+
+    testImplementation("com.h2database:h2")
+    testImplementation("org.junit.jupiter:junit-jupiter-engine")
+    testImplementation("org.junit.jupiter:junit-jupiter-params")
+    testImplementation("org.assertj:assertj-core")
+    testImplementation("org.mockito:mockito-junit-jupiter")
+
+    testImplementation("org.testcontainers:junit-jupiter")
+    testImplementation("org.testcontainers:postgresql")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
