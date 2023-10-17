1e03fcb1cbc89bc05d58099e243cab053c8c585b
Kakaulin Aleksei <m9cnik17@yandex.ru>
2023-10-17 14:00:06 +0700

    Домашнее задание

--- hw11-Ehcache/src/main/java/ru/otus/cachehw/MyCache.java
+++ hw11-Ehcache/src/main/java/ru/otus/cachehw/MyCache.java
@@ -1,26 +1,31 @@
 package ru.otus.cachehw;
 
+
+import java.util.Map;
+import java.util.WeakHashMap;
+
 public class MyCache<K, V> implements HwCache<K, V> {
     // Надо реализовать эти методы
+    private final Map<K, V> cacheMap = new WeakHashMap<>();
 
     @Override
     public void put(K key, V value) {
-        throw new UnsupportedOperationException();
+        cacheMap.put(key, value);
     }
 
     @Override
     public void remove(K key) {
-        throw new UnsupportedOperationException();
+        cacheMap.remove(key);
     }
 
     @Override
     public V get(K key) {
-        throw new UnsupportedOperationException();
+        return cacheMap.get(key);
     }
 
     @Override
     public void addListener(HwListener<K, V> listener) {
-        throw new UnsupportedOperationException();
+       throw new UnsupportedOperationException();
     }
 
     @Override
--- hw11-Ehcache/src/main/java/ru/otus/crm/service/DbServiceClientImpl.java
+++ hw11-Ehcache/src/main/java/ru/otus/crm/service/DbServiceClientImpl.java
@@ -2,6 +2,8 @@ package ru.otus.crm.service;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import ru.otus.cachehw.HwCache;
+import ru.otus.cachehw.MyCache;
 import ru.otus.core.repository.DataTemplate;
 import ru.otus.core.sessionmanager.TransactionManager;
 import ru.otus.crm.model.Client;
@@ -9,11 +11,17 @@ import ru.otus.crm.model.Client;
 import java.util.List;
 import java.util.Optional;
 
+import static java.lang.String.valueOf;
+import static java.util.Objects.nonNull;
+
 public class DbServiceClientImpl implements DBServiceClient {
     private static final Logger log = LoggerFactory.getLogger(DbServiceClientImpl.class);
 
     private final DataTemplate<Client> clientDataTemplate;
     private final TransactionManager transactionManager;
+
+    private final HwCache<String, Client> cache = new MyCache<>();
+
 
     public DbServiceClientImpl(TransactionManager transactionManager, DataTemplate<Client> clientDataTemplate) {
         this.transactionManager = transactionManager;
@@ -37,9 +45,17 @@ public class DbServiceClientImpl implements DBServiceClient {
 
     @Override
     public Optional<Client> getClient(long id) {
+        Client cachedClient = cache.get(valueOf(id));
+        if (nonNull(cachedClient)) {
+            return Optional.of(cachedClient);
+        }
+
         return transactionManager.doInReadOnlyTransaction(session -> {
             var clientOptional = clientDataTemplate.findById(session, id);
-            log.info("client: {}", clientOptional);
+             log.info("client: {}", clientOptional);
+
+            clientOptional.ifPresent(c -> cache.put(Long.toString(id), c));
+
             return clientOptional;
         });
     }
