7b6ba4db6f3bdc4cc7f9168737c7f7357943897c
Kakaulin Aleksei <m9cnik17@yandex.ru>
2023-10-18 20:58:47 +0700

    Функционал слушателя

--- hw11-Ehcache/src/main/java/ru/otus/cachehw/MyCache.java
+++ hw11-Ehcache/src/main/java/ru/otus/cachehw/MyCache.java
@@ -1,35 +1,47 @@
 package ru.otus.cachehw;
 
 
-import java.util.Map;
-import java.util.WeakHashMap;
+import java.util.*;
 
 public class MyCache<K, V> implements HwCache<K, V> {
-    // Надо реализовать эти методы
+
+
+    private static final String ACTION_PUT = "PUT";
+    private static final String ACTION_GET = "GET";
+    private static final String ACTION_REMOVE = "REMOVE";
     private final Map<K, V> cacheMap = new WeakHashMap<>();
+    private final Set<HwListener<K,V>> listenersSet = new HashSet<>();
 
     @Override
     public void put(K key, V value) {
         cacheMap.put(key, value);
+        notifyListeners(key, value, ACTION_PUT);
     }
 
     @Override
     public void remove(K key) {
         cacheMap.remove(key);
+        notifyListeners(key, null, ACTION_REMOVE);
     }
 
     @Override
     public V get(K key) {
-        return cacheMap.get(key);
+        V result = cacheMap.get(key);
+        notifyListeners(key, result, ACTION_GET);
+        return result;
     }
 
     @Override
     public void addListener(HwListener<K, V> listener) {
-       throw new UnsupportedOperationException();
+        listenersSet.add(listener);
     }
 
     @Override
     public void removeListener(HwListener<K, V> listener) {
-        throw new UnsupportedOperationException();
+        listenersSet.remove(listener);
     }
+    private void notifyListeners(K key, V value, String cacheListenerAction) {
+        listenersSet.forEach(listener -> listener.notify(key, value, cacheListenerAction));
+    }
+
 }
