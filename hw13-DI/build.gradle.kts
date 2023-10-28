6ff17827a1f33ce32b1d2b71e85e6a01906fca43
Kakaulin Aleksei <m9cnik17@yandex.ru>
2023-10-28 16:40:58 +0700

    дз

--- hw13-DI/build.gradle.kts
+++ hw13-DI/build.gradle.kts
@@ -5,12 +5,11 @@ plugins {
 group = "ru.otus"
 
 dependencies {
-    implementation("org.reflections:reflections")
+
 
     testImplementation("org.junit.jupiter:junit-jupiter-engine")
     testImplementation("org.junit.jupiter:junit-jupiter-params")
     testImplementation("org.assertj:assertj-core")
-    testImplementation("org.mockito:mockito-junit-jupiter")
 
 }
 
--- hw13-DI/src/main/java/ru/otus/appcontainer/AppComponentsContainerImpl.java
+++ hw13-DI/src/main/java/ru/otus/appcontainer/AppComponentsContainerImpl.java
@@ -1,12 +1,13 @@
 package ru.otus.appcontainer;
 
+import ru.otus.appcontainer.api.AppComponent;
 import ru.otus.appcontainer.api.AppComponentsContainer;
-import ru.otus.appcontainer.api.AppComponentsContainerConfig;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+
+import java.lang.reflect.Method;
+import java.lang.reflect.Parameter;
+import java.util.*;
+
 
 @SuppressWarnings("squid:S1068")
 public class AppComponentsContainerImpl implements AppComponentsContainer {
@@ -15,27 +16,68 @@ public class AppComponentsContainerImpl implements AppComponentsContainer {
     private final Map<String, Object> appComponentsByName = new HashMap<>();
 
     public AppComponentsContainerImpl(Class<?> initialConfigClass) {
-        processConfig(initialConfigClass);
-    }
-
-    private void processConfig(Class<?> configClass) {
-        checkConfigClass(configClass);
-        // You code here...
-    }
-
-    private void checkConfigClass(Class<?> configClass) {
-        if (!configClass.isAnnotationPresent(AppComponentsContainerConfig.class)) {
-            throw new IllegalArgumentException(String.format("Given class is not config %s", configClass.getName()));
+        try {
+            processConfig(initialConfigClass);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
         }
     }
 
+    private void processConfig(Class<?> configClass) throws Exception {
+        Object configClassInstance = configClass.getConstructor().newInstance();
+
+        List<Method> methods = Arrays.stream(configClass.getMethods()).filter(method -> method.isAnnotationPresent(AppComponent.class))
+                .sorted(Comparator.comparingInt(m -> m.getAnnotation(AppComponent.class).order())).toList();
+
+        String componentName;
+        Object component;
+        for (Method method : methods) {
+            componentName = getComponentName(method);
+            checkDuplicate(componentName);
+            component = method.invoke(configClassInstance, getArgs(method.getParameters()));
+
+            appComponentsByName.put(componentName, component);
+            appComponents.add(component);
+        }
+    }
+
+
     @Override
+    @SuppressWarnings("unchecked")
     public <C> C getAppComponent(Class<C> componentClass) {
-        return null;
+        List<Object> components = appComponents.stream().filter(component -> componentClass.isAssignableFrom(component.getClass())).toList();
+        if (components.size() > 1) {
+            throw new AppContainerException("getting component is present in container more then one instance");
+        } else if (components.isEmpty()) {
+            throw new AppContainerException("getting component is missing in container");
+        }
+        return (C) components.get(0);
     }
 
     @Override
+    @SuppressWarnings("unchecked")
     public <C> C getAppComponent(String componentName) {
-        return null;
+        return (C) Optional.ofNullable(appComponentsByName.get(componentName)).orElseThrow(() -> new AppContainerException("getting component is absent in container"));
     }
+
+
+    private String getComponentName(Method method) {
+        return method.getAnnotation(AppComponent.class).name();
+    }
+
+    private void checkDuplicate(String componentName) {
+        if (appComponentsByName.containsKey(componentName)) {
+            throw new AppContainerException("duplicate component name: " + componentName);
+        }
+    }
+
+    private Object[] getArgs(Parameter[] parameters) {
+        Object[] args = new Object[parameters.length];
+        for (int i = 0; i < args.length; i++) {
+            args[i] = getAppComponent(parameters[i].getType());
+        }
+        return args;
+    }
+
+
 }
