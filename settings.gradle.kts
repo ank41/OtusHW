e1ce453c8cd0ee2088b979926a6ceda999e98b03
Aleksei Kakaulin <m9cnik17@yandex.ru>
2023-11-09 20:07:58 +0700

    ресурсы

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -14,6 +14,7 @@ include("hw10-Hibernate")
 include("hw11-Ehcache")
 include("hw12-web-server")
 include("hw13-DI")
+include("hw14-SDJdbc")
 
 pluginManagement {
     val jgitver: String by settings
@@ -36,4 +37,4 @@ pluginManagement {
         id("com.diffplug.spotless") version spotless
     }
 }
-include("hw14-SDJdbc")
+
